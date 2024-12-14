4fcde614f44fe579321e733dfb0c234af86f2b48
Brayden Nilson <nilson.brayden@gmail.com>
2024-12-13 22:02:05 -0800

    cleanup

--- day_3/day_3.R
+++ day_3/day_3.R
@@ -1,3 +1,4 @@
+# Generic function for outputing answer files
 write_answer <- function(x, part) {
   out_file <- paste0("answer_", part, ".txt")
   x |>
@@ -6,26 +7,30 @@ write_answer <- function(x, part) {
 }
 
 # Part 1 ---------------------------------------
+##  What do you get if you add up all of the
+##  results of the multiplications?
 
 input <- "input.txt" |>
   readLines() |>
   paste(collapse = "")
 
+# Get all occurences of "mul(\d*?,\d*?)"
 extract_valid_muls <- function(lines) {
   pattern <- "mul\\(\\d*?,\\d*?\\)"
   lines |>
     stringr::str_extract_all(pattern) |>
     unlist()
 }
-
+# For each occurence of muls, multiply values
 do_mul <- function(muls) {
-  mul_row <- \(mul) as.numeric(mul[1]) * as.numeric(mul[2])
   muls |>
     stringr::str_remove_all("mul\\(|\\)") |>
     stringr::str_split(",", simplify = TRUE) |>
-    apply(1, mul_row)
-}
+apply(2, as.numeric) |>
+  apply(1, \(vals) vals[1] * vals[2])
 
+}
+# Extract valid muls, multiply vals, sum
 input |>
   extract_valid_muls() |>
   do_mul() |>
@@ -33,7 +38,9 @@ input |>
   write_answer(part = 1)
 
 # Part 2 ---------------------------------------
+## What about when only including muls between do() and don't()?
 
+# Search for valid muls between do() and don't()
 extract_enabled_muls <- function(lines) {
   pattern <- "do\\(\\).*?don't\\(\\)"
   paste0("do()", lines, "don't()") |>
@@ -42,6 +49,7 @@ extract_enabled_muls <- function(lines) {
     extract_valid_muls()
 }
 
+# Get enabled muls, multiply vals, sum
 input |>
   extract_enabled_muls() |>
   do_mul() |>
