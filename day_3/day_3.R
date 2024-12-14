8497ef798425952641d4ca900d894a517b32f883
Brayden Nilson <nilson.brayden@gmail.com>
2024-12-13 18:12:30 -0800

    complete part 2

--- day_3/day_3.R
+++ day_3/day_3.R
@@ -8,7 +8,8 @@ write_answer <- function(x, part) {
 # Part 1 ---------------------------------------
 
 input <- "input.txt" |>
-  readLines()
+  readLines() |>
+  paste(collapse = "")
 
 extract_valid_muls <- function(lines) {
   pattern <- "mul\\(\\d*?,\\d*?\\)"
@@ -22,8 +23,7 @@ do_mul <- function(muls) {
   muls |>
     stringr::str_remove_all("mul\\(|\\)") |>
     stringr::str_split(",", simplify = TRUE) |>
-apply(1, mul_row)
-
+    apply(1, mul_row)
 }
 
 input |>
@@ -33,3 +33,17 @@ input |>
   write_answer(part = 1)
 
 # Part 2 ---------------------------------------
+
+extract_enabled_muls <- function(lines) {
+  pattern <- "do\\(\\).*?don't\\(\\)"
+  paste0("do()", lines, "don't()") |>
+    stringr::str_extract_all(pattern) |>
+    unlist() |>
+    extract_valid_muls()
+}
+
+input |>
+  extract_enabled_muls() |>
+  do_mul() |>
+  sum() |>
+  write_answer(part = 2)
