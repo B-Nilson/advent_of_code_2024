7dd40f54b5ccb15bf11e402a9b1190adf95469c7
Brayden Nilson <nilson.brayden@gmail.com>
2024-12-13 17:31:54 -0800

    complete part_1

--- day_2/day_2.R
+++ day_2/day_2.R
@@ -8,6 +8,23 @@ write_answer <- function(x, part) {
 # Part 1 ---------------------------------------
 
 input <- "input.txt" |>
-  data.table::fread()
+  readLines()
+
+reports <- input |>
+  stringr::str_split(" ")
+
+is_safe <- function(levels) {
+  levels <- as.numeric(levels)
+  changes <- (levels - dplyr::lag(levels))[-1]
+  is_0_or_gt_3 <- abs(changes) > 3 | abs(changes) < 1
+  is_both_neg_and_pos <- any(changes < 0) & any(changes > 0)
+  !(any(is_0_or_gt_3) | is_both_neg_and_pos)
+}
+
+reports |>
+  sapply(is_safe) |>
+  sum() |>
+  write_answer(part = 1)
+
 
 # Part 2 ---------------------------------------
