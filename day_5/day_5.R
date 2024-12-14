90fa2316e30866de382a7ed8d2dbd7612f0d52c4
Brayden Nilson <nilson.brayden@gmail.com>
2024-12-13 22:49:39 -0800

    cleanup part 1

--- day_5/day_5.R
+++ day_5/day_5.R
@@ -7,47 +7,46 @@ write_answer <- function(x, part) {
 }
 
 # Part 1 ---------------------------------------
-## What do you get if you add up the middle page 
+## What do you get if you add up the middle page
 ##   number from the correctly-ordered updates?
 
 input <- "input.txt" |>
   readLines()
 
+# Seperate rule and update entries in input
 rule_entries <- 1:(which(input == "") - 1)
 rules <- input[rule_entries] |>
   stringr::str_split("\\|", simplify = TRUE) |>
   as.data.frame() |>
   setNames(c("X", "Y"))
-
 update_entries <- (which(input == "") + 1):length(input)
 updates <- input[update_entries] |>
   stringr::str_split(",") |>
   lapply(as.numeric)
 
+# Checks if an update matches the page order rules
 check_rules <- function(update, rules) {
-  rules |> apply(1, \(rule) {
-    a_loc <- which(update == rule[1])
-    b_loc <- which(update == rule[2])
-    if (length(c(a_loc, b_loc)) != 2) {
-      return(TRUE)
-    }
-    a_loc < b_loc
-  }) |>
+  rules |>
+    apply(1, \(rule) {
+      a_loc <- which(update == rule[1])
+      b_loc <- which(update == rule[2])
+      if (length(c(a_loc, b_loc)) != 2) {
+        return(TRUE)
+      }
+      a_loc < b_loc
+    }) |>
     all()
 }
 
-valid_updates = updates |>
+# Check for valid updates
+valid_updates <- updates |>
   sapply(check_rules, rules = rules)
-
-middle = function(x) {
-  x[median(1:length(x))]
-}
-
+# Get the middle number from those and sum
 updates[which(valid_updates)] |>
-  sapply(middle) |>
-  sum()
+  sapply(\(x) x[median(1:length(x))]) |>
+  sum() |>
+  write_answer(part = 1)
 
-# write_answer(part = 1)
 # Part 2 ---------------------------------------
 
 # write_answer(part = 2)
