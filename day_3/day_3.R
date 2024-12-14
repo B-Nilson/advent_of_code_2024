569d8636b1d1eb95a069ff91406dfe5b037dd40d
Brayden Nilson <nilson.brayden@gmail.com>
2024-12-13 17:42:03 -0800

    init day 3

---
+++ day_3/day_3.R
@@ -0,0 +1,14 @@
+write_answer <- function(x, part) {
+  out_file <- paste0("answer_", part, ".txt")
+  x |>
+    as.character() |>
+    writeLines(out_file)
+}
+
+# Part 1 ---------------------------------------
+
+
+
+
+
+# Part 2 ---------------------------------------
