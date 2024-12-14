850b19a0e08ef8a71f9ec6acaf3a422d263c0922
Brayden Nilson <nilson.brayden@gmail.com>
2024-12-13 17:56:55 -0800

    complete part 1

--- day_3/day_3.R
+++ day_3/day_3.R
@@ -7,8 +7,28 @@ write_answer <- function(x, part) {
 
 # Part 1 ---------------------------------------
 
+input <- "input.txt" |>
+  readLines()
 
+extract_valid_muls = function(lines) {
+  pattern = "mul\\(\\d*?,\\d*?\\)"
+  lines |>
+    stringr::str_extract_all(pattern, simplify = TRUE) |>
+    as.vector()
+} 
 
+do_mul = function(muls) {
+  muls |>
+    stringr::str_remove_all("mul\\(|\\)") |>
+    stringr::str_split(",", simplify = TRUE) |>
+    apply(2, as.numeric) |>
+    apply(1, \(mul) mul[1] * mul[2])
+}
 
+input |>
+  extract_valid_muls() |>
+  do_mul() |>
+  sum() |>
+  write_answer(part = 1)
 
 # Part 2 ---------------------------------------
