8583af55de0c1bd9b4d7637c6cce890eb0774cd3
Brayden Nilson <nilson.brayden@gmail.com>
2024-12-13 21:44:25 -0800

    cleanup

--- day_4/day_4.R
+++ day_4/day_4.R
@@ -1,3 +1,6 @@
+# Functions ---------------------------------------
+
+# Generic function for outputing answer files
 write_answer <- function(x, part) {
   out_file <- paste0("answer_", part, ".txt")
   x |>
@@ -5,24 +8,23 @@ write_answer <- function(x, part) {
     writeLines(out_file)
 }
 
+# Get diagonals in a single direction for a matrix
 get_diagonals <- function(matrix) {
-  ndim <- nrow(matrix)
-  offsets <- seq(-ndim + 1, ndim - 1)
-  offsets |>
-    lapply(\(i) {
-      if (i >= 0) { # Extract diagonal above or on the main diagonal
-        (1:(ndim - i)) |>
-          sapply(\(j) matrix[j, j + i]) |>
-          paste(collapse = "")
-      } else { # Extract diagonal below the main diagonal
-        (1:(ndim + i)) |>
-          sapply(\(j) matrix[j - i, j]) |>
-          paste(collapse = "")
+  rows <- nrow(matrix)
+  seq(-rows + 1, rows - 1) |>
+    sapply(\(i) {
+      if (i >= 0) {
+        diag <- (1:(rows - i)) |>
+          sapply(\(j) matrix[j, j + i])
+      } else {
+        diag <- (1:(rows + i)) |>
+          sapply(\(j) matrix[j - i, j])
       }
-    }) |>
-    unlist()
+      diag |> paste(collapse = "")
+    })
 }
 
+# Rotate a matrix 90 degrees multiple times
 rotate <- function(matrix, times = 1) {
   times <- times %% 4
   if (times == 0) {
@@ -33,77 +35,67 @@ rotate <- function(matrix, times = 1) {
   return(matrix)
 }
 
-# Part 1 ---------------------------------------
+# Check if a matrix location matches a X-MAS pattern
+check_if_x_mas <- function(matrix, x, y) {
+  # Predefined X-MAS patterns
+  patterns <- list(
+    c("M.S", ".A.", "M.S"),
+    c("M.M", ".A.", "S.S"),
+    c("S.S", ".A.", "M.M"),
+    c("S.M", ".A.", "S.M")
+  )
+  # Handle edge cases
+  rows <- (x - 1):(x + 1)
+  cols <- (y - 1):(y + 1)
+  invalid_rows <- rows < 0 | rows > nrow(matrix)
+  invalid_cols <- cols < 0 | cols > ncol(matrix)
+  if (any(invalid_rows | invalid_cols)) {
+    return(FALSE)
+  }
+  # Check if this location matches any patterns
+  sample <- matrix[rows, cols] |> paste(collapse = "")
+  patterns <- patterns |> sapply(paste, collapse = "")
+  patterns |>
+    sapply(\(pattern) sample |> stringr::str_detect(pattern)) |>
+    any()
+}
 
-input <- "input.txt" |>
-  readLines()
+# Part 1 ---------------------------------------
+## Look for "XMAS" in a word search
+
+input <- "input.txt" |> readLines()
 
 word_search <- input |>
   stringr::str_split("", simplify = TRUE)
 
+# Get all vertical, horizontal, and diagonal text
+#   in both directions
 verticals <- word_search |>
-  # Include right -> left
   cbind(word_search[nrow(word_search):1, ]) |>
   apply(2, paste, collapse = "")
 
 horizontals <- word_search |>
-  # Include bottom -> top
   rbind(word_search[, ncol(word_search):1]) |>
   apply(1, paste, collapse = "")
 
-diagnols <- 0:3 |> # Get all 4 sets of diagnols
+diagonals <- 0:3 |>
   lapply(\(times) word_search |>
     rotate(times) |>
     get_diagonals()) |>
   unlist()
 
 # Combine and count occurences of "XMAS"
-c(verticals, horizontals, diagnols) |>
+c(verticals, horizontals, diagonals) |>
   stringr::str_count("XMAS") |>
   sum() |>
   write_answer(part = 1)
 
 # Part 2 ---------------------------------------
+## Look for "X-MAS" (as in two "MAS" shaped like an "X")
 
-is_a <- (word_search == "A") |>
-  which(arr.ind = TRUE)
-
-patterns <- list(
-  c("M.S", ".A.", "M.S"),
-  c("M.M", ".A.", "S.S"),
-  c("S.S", ".A.", "M.M"),
-  c("S.M", ".A.", "S.M")
-)
-
-filter <- function(matrix, x, y, patterns) {
-  rows <- (x - 1):(x + 1)
-  cols <- (y - 1):(y + 1)
-  if (any(
-    rows < 0 | rows > nrow(matrix) |
-      cols < 0 | cols > ncol(matrix)
-  )) {
-    return(FALSE)
-  }
-  grid <- matrix[rows, cols] |>
-    paste(collapse = "")
-  patterns |>
-    sapply(\(pattern){
-      pattern <- pattern |>
-        paste(collapse = "")
-      grid |>
-        stringr::str_detect(pattern)
-    }) |>
-    any()
-}
-
-1:nrow(is_a) |>
-  sapply(\(i) {
-    word_search |>
-      filter(
-        x = is_a[i, 1],
-        y = is_a[i, 2],
-        patterns = patterns
-      )
-  }) |>
+# Count locations centered on "A" with matches to X-MAS patterns
+which(word_search == "A", arr.ind = TRUE) |>
+  apply(1, \(a_loc) word_search |>
+    check_if_x_mas(a_loc[1], a_loc[2])) |>
   sum() |>
   write_answer(part = 2)
