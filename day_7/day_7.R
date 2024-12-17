7bda8d3e130aca83c3924243b745d3593ee5fad1
Brayden Nilson <nilson.brayden@gmail.com>
2024-12-16 19:04:23 -0800

    cleanup part 1

--- day_7/day_7.R
+++ day_7/day_7.R
@@ -7,21 +7,22 @@ write_answer <- function(x, part) {
 }
 
 # Part 1 ---------------------------------------
+## Determine which equations could possibly be true. 
+## What is their total calibration result?
 
 input <- "input.txt" |>
   readLines()
 
 check_equation = function(answer, values, operators) {
   add_brackets = function(x) {
-    paste0("(", x, ")")
+    paste0("(", x, ")") # so evals from left -> right
   }
   # Init results with first value
   results = data.frame(
-    expression = values[1] |> add_brackets(),
-    total = values[1]
+    expression = values[1] |> add_brackets()
   )
   # Handle first value larger than answer
-  if(results$total > answer){
+  if(values[1] > answer){
     return(FALSE)
   }
   # For all other values
@@ -40,7 +41,7 @@ check_equation = function(answer, values, operators) {
         total = expression |>
           sapply(\(e) eval(parse(text = e)))
     ) |> 
-      # Drop any that exceed the total 
+      # Drop any that exceed the expected total 
       dplyr::filter(total <= answer)
   }
   # Evaluate if answer possible
@@ -54,12 +55,14 @@ input |>
       stringr::str_split(": | ", simplify = TRUE) |>
       as.numeric()
     is_valid = check_equation(
-      answer = inputs[1], values = inputs[-1], operators)
-    ifelse(is_valid, inputs[1], 0)
+      answer = inputs[1], 
+      values = inputs[-1], 
+      operators
+    )
+    is_valid |> ifelse(inputs[1], 0)
   }) |>
   sum() |>
   write_answer(part = 1)
-
 
 # Part 2 ---------------------------------------
 
