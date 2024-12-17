6cb8744a2c3dd8ff44901c8da68aa791ab85b59e
Brayden Nilson <nilson.brayden@gmail.com>
2024-12-16 21:47:09 -0800

    hmm, not sure why this doesn't work. it passes the test examples, need to do some samples

--- day_7/day_7.R
+++ day_7/day_7.R
@@ -6,6 +6,15 @@ write_answer <- function(x, part) {
     writeLines(out_file)
 }
 
+eval_math = function(x){
+  parse(text = paste0(
+       "c(",
+       x |> stringi::stri_c(collapse = ", "),
+       ")"
+      )) |> 
+    eval()
+}
+
 # Part 1 ---------------------------------------
 ## Determine which equations could possibly be true. 
 ## What is their total calibration result?
@@ -19,7 +28,8 @@ check_equation = function(answer, values, operators) {
   }
   # Init results with first value
   results = data.frame(
-    expression = values[1] |> add_brackets()
+    expression = values[1] |> add_brackets(),
+    full_expresion = values[1] |> add_brackets()
   )
   # Handle first value larger than answer
   if(values[1] > answer){
@@ -31,26 +41,34 @@ check_equation = function(answer, values, operators) {
       # Expand out all combos of operators
       dplyr::mutate(next_value = values[i]) |>
       tidyr::expand(
-        tidyr::nesting(expression, next_value),
+        tidyr::nesting(expression, full_expresion, next_value),
         operator = operators) |>
       # Add to the expression(s) and retotal
       dplyr::mutate(
-        expression = expression |>
+        full_expresion = full_expresion |>
           paste(operator, next_value) |>
           add_brackets(),
-        total = expression |>
-          sapply(\(e) eval(parse(text = e)))
-    ) |> 
+        expression = expression |>
+          paste(operator, next_value) |>
+          add_brackets() |>
+          handle_concat()
+      )
+    results = results |>
+      dplyr::mutate(total = expression |> eval_math()) |> 
       # Drop any that exceed the expected total 
       dplyr::filter(total <= answer)
+    if(answer %in% results$total) {
+      break
+    }
   }
   # Evaluate if answer possible
   answer %in% results$total
 }
 
 operators = c("+", "*")
-input |>
+valid_values = input |>
   sapply(\(line){
+    cat(line, "\n")
     inputs = line |>
       stringr::str_split(": | ", simplify = TRUE) |>
       as.numeric()
@@ -60,10 +78,49 @@ input |>
       operators
     )
     is_valid |> ifelse(inputs[1], 0)
-  }) |>
+  })
+valid_values |>
   sum() |>
   write_answer(part = 1)
 
 # Part 2 ---------------------------------------
 
-# write_answer(part = 2)
+handle_concat = function(x, concat = "\\|\\|") {
+  has_concat = stringr::str_detect(x, concat)
+  if(!any(has_concat)) {
+    return(x)
+  }
+  # Eval sides of concat entries
+  sides = x[has_concat] |>
+    stringr::str_split(concat, simplify = TRUE)
+  sides[, 1] = sides[, 1] |> 
+    paste0(")") |>
+    eval_math()
+  sides[, 2] = sides[, 2] |> 
+    stringr::str_remove_all("\\)$|^ ")
+  # Insert pasted entries
+  x[has_concat] = paste0(
+    sides[, 1],
+    sides[, 2]
+  )
+  return(x)
+}
+
+operators = c("+", "*", "||")
+valid_concat_values = input[valid_values == 0] |>
+  sapply(\(line){
+    cat(line, "\n")
+    inputs = line |>
+      stringr::str_split(": | ", simplify = TRUE) |>
+      as.numeric()
+    if(sum(inputs[-1] > inputs[1])) return(0)
+    is_valid = check_equation(
+      answer = inputs[1], 
+      values = inputs[-1], 
+      operators
+    )
+    is_valid |> ifelse(inputs[1], 0)
+  })
+
+(sum(valid_concat_values) + sum(valid_values)) |>
+  write_answer(part = 2)
